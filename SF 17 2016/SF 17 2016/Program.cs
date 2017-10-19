80aa12909c6185f453c815042a73ff4f9eb85841
Kurtek Du�an <SF17-2016@ftn.ns>
2017-10-19 14:18:05 +0200

    init

---
+++ SF 17 2016/SF 17 2016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF_17_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
